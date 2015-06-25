ae7d7f23f1ed027f717a010530f5c24c92347a54
Ryan Corn <cornr@email.uscb.edu>
2015-06-25 16:40:09 -0400

    added delete function, needs work

--- js/todo.js
+++ js/todo.js
@@ -9,7 +9,7 @@ document.observe("dom:loaded", function() {
         numTask = parseInt(localStorage.getItem('numTask'));
         for (var i = 1; i <= numTask; i++){
             toDo.insertLi(i);
-            $('input_' + i).setValue(localStorage.getItem('task'+ i));
+            $('input_' + i).setValue(localStorage.getItem('task_'+ i));
         }
     }
 
@@ -20,6 +20,8 @@ document.observe("dom:loaded", function() {
     $('save').observe('click', function(e) {
         toDo.save();
     });
+
+    $$('.deleteTask').invoke('observe', 'click', toDo.delete)
 });
 
 var ToDoList = Class.create();
@@ -29,16 +31,24 @@ ToDoList.prototype = {
     insertLi: function(numTask) {
         $('sortable').insert('<li  id="task_'+
         numTask + '" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><input id="input_' +
-        numTask + '" size="60" type="text"></li>')
+        numTask + '" size="60" type="text"><button class="deleteTask" type="button">x</button></li>')
     },
     save: function(){
         localStorage.setItem('numTask', jQuery('li').length);
         var numTask = parseInt(localStorage.getItem('numTask'));
         for (var i = 1; i <= numTask; i++ ){
-            localStorage.setItem('task' + i, $('input_' + i).value );
+            localStorage.setItem('task_' + i, $('input_' + i).value );
         }
 
         var sorted = jQuery( "#sortable" ).sortable( "serialize");
         localStorage.setItem('sorted', sorted);
+    },
+    delete: function(){
+        var taskLi = this.up(0);
+        console.log(taskLi);
+
+        localStorage.removeItem(taskLi.identify());
+        taskLi.remove();
+        localStorage.setItem('numTask', jQuery('li').length);
     }
 };
