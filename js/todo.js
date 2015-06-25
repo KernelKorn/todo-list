aaceae1d7688ac961edf11c3ec2e9fa79a9d4b0d
Ryan Corn <cornr@email.uscb.edu>
2015-06-25 14:57:05 -0400

    added save function

--- js/todo.js
+++ js/todo.js
@@ -1,31 +1,44 @@
 document.observe("dom:loaded", function() {
     var numTask;
+    var toDo = new ToDoList()
 
     if (localStorage.getItem('numTask') === null){
         numTask = 1;
-        console.log(numTask);
+        toDo.insertLi(numTask);
     } else {
-        var i;
-        numTask = localStorage.getItem('numTask');
-        for (i = 2; i <= numTask; i++){
-            insertLi(i);
+        numTask = parseInt(localStorage.getItem('numTask'));
+        for (var i = 1; i <= numTask; i++){
+            toDo.insertLi(i);
+            $('input_' + i).setValue(localStorage.getItem('task'+ i));
         }
     }
 
-    $('task1').setValue(localStorage.getItem('task1'));
-
     $('addTask').observe('click', function(e) {
-       insertLi(numTask+=1);
+       toDo.insertLi(numTask+=1);
     });
 
     $('save').observe('click', function(e) {
-       localStorage.setItem('numTask', jQuery('li').length);
-       localStorage.setItem('task1', $('task1').value );
+        toDo.save();
     });
-
 });
 
-function insertLi(numTask) {
-    $('sortable').insert('<li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><input id="task'+
-    (numTask) + '" size="60" type="text"></li>')
-}
+var ToDoList = Class.create();
+ToDoList.prototype = {
+    initialize: function() {
+    },
+    insertLi: function(numTask) {
+        $('sortable').insert('<li  id="task_'+
+        numTask + '" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><input id="input_' +
+        numTask + '" size="60" type="text"></li>')
+    },
+    save: function(){
+        localStorage.setItem('numTask', jQuery('li').length);
+        var numTask = parseInt(localStorage.getItem('numTask'));
+        for (var i = 1; i <= numTask; i++ ){
+            localStorage.setItem('task' + i, $('input_' + i).value );
+        }
+
+        var sorted = jQuery( "#sortable" ).sortable( "serialize");
+        localStorage.setItem('sorted', sorted);
+    }
+};
