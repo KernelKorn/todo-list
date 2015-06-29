fce2213fc7e9ca4170eac65d57b923cbcda5d313
Ryan Corn <cornr@email.uscb.edu>
2015-06-29 16:13:38 -0400

    add comments

--- js/todo.js
+++ js/todo.js
@@ -2,24 +2,29 @@ document.observe("dom:loaded", function() {
     var numTask;
     var toDo = new ToDoList()
 
+    // checks to see if local storage exist if it does sets numTask to 1 so there is at least one input
+    // else it takes the data from local stotage and displays it
     if (localStorage.getItem('numTask') === null || localStorage.getItem('numTask') == 0){
         numTask = 1;
         toDo.insertLi(numTask);
     } else {
         var todoTitle = localStorage.getItem('title');
-
         if(todoTitle === null){
             todoTitle = "Click to add Title";
         }
+        // sets the title of the to do list
         $('todoTitle').update(todoTitle);
-        numTask = parseInt(localStorage.getItem('numTask'));
 
+        // finds the number of task then inerates through them setting data to correct input
+        numTask = parseInt(localStorage.getItem('numTask'));
         for (var i = 1; i <= numTask; i++){
             toDo.insertLi(i);
+            // finds task in local storage by key
             var taskObject = JSON.parse(localStorage.getItem( localStorage.key( i )));
             $('input_' + i).setValue( taskObject['taskValue']);
             $('div_' + i).update( taskObject['taskValue'].strike());
 
+            // checks to see if task is complete and how to display it
             if(taskObject['isCompleted'] === "true"){
                 $('input_' + i).hide()
                 $('div_' + i).show();
@@ -30,12 +35,13 @@ document.observe("dom:loaded", function() {
         }
     }
 
+    // Observer for changing title
     $('todoTitle').observe('click',function(e){
         $('inputTitle').show();
         $('saveTitleBtn').show();
         $('todoTitle').hide();
     });
-
+    // Observer to save title
     $('saveTitleBtn').observe('click', function(e){
         var titleValue = $('inputTitle').value;
 
@@ -51,10 +57,6 @@ document.observe("dom:loaded", function() {
        toDo.insertLi(numTask+=1);
     });
 
-    //document.observe('click', function(e) {
-    //    toDo.saveTasks();
-    //});
-
     jQuery('#sortable').on('keyup','input', function(e) {
         if (event.keyCode == 13) {
             toDo.saveTasks();
@@ -68,8 +70,6 @@ document.observe("dom:loaded", function() {
     jQuery('#sortable').on('click','.deleteTaskBtn',function(e) {
         toDo.deleteTask(this);
     });
-    //$$('.deleteTaskBtn').invoke('observe', 'click', toDo.deleteTask);
-    //$$('.finishTaskBtn').invoke('on', 'click','.finishTaskBtn', toDo.finishTask);
 });
 
 var ToDoList = Class.create();
@@ -77,6 +77,7 @@ ToDoList.prototype = {
     initialize: function() {
     },
     insertLi: function(numTask) {
+        // inserts list item
         $('sortable').insert('<li  id="task_'+
         numTask + '" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><input id="input_' +
         numTask + '" size="45" type="text"><div id="div_' + numTask +'" ></div><button class="finishTaskBtn" type="button">&#10003</button>' +
@@ -84,10 +85,10 @@ ToDoList.prototype = {
     },
     saveTasks: function(){
         var taskObject;
-
         localStorage.setItem('numTask', jQuery('li').length);
         var numTask = parseInt(localStorage.getItem('numTask'));
 
+        // iterates through all list items and saves the data to the correct task
         for (var i = 1; i <= numTask; i++ ){
             taskObject =  JSON.parse(localStorage.getItem('task_' + i));
             if( taskObject === null || taskObject['isCompleted'] === "false"){
@@ -105,6 +106,9 @@ ToDoList.prototype = {
         localStorage.removeItem(taskLi.identify());
         taskLi.remove();
         localStorage.setItem('numTask', jQuery('li').length);
+
+        var sorted = jQuery("#sortable").sortable("serialize");
+        localStorage.setItem('z_sorted', sorted);
     },
     finishTask: function(child){
         var taskID = child.up(0).identify().replace('task_','');
