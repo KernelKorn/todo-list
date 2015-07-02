b5771f1acc0d744825136e8aa71c3a5b3440ac72
Ryan Corn <cornr@email.uscb.edu>
2015-07-02 14:39:31 -0400

    Changes made after Code Review

--- js/todo.js
+++ js/todo.js
@@ -1,142 +1,149 @@
 document.observe("dom:loaded", function() {
-    var numTask;
-    var toDo = new ToDoList()
-
-    // checks to see if local storage exist if it does sets numTask to 1 so there is at least one input
-    // else it takes the data from local stotage and displays it
-    if (localStorage.getItem('numTask') === null || localStorage.getItem('numTask') == 0){
-        numTask = 1;
-        toDo.insertLi(numTask);
-    } else {
-        var todoTitle = localStorage.getItem('title');
-        if(todoTitle === null){
-            todoTitle = "Click to add Title";
-        }
-        // sets the title of the to do list
-        $('todoTitle').update(todoTitle);
-
-        // finds the number of task then inerates through them setting data to correct input
-        numTask = parseInt(localStorage.getItem('numTask'));
-        for (var i = 1; i <= numTask; i++){
-            toDo.insertLi(i);
-            // finds task in local storage by key
-            var taskObject = JSON.parse(localStorage.getItem( localStorage.key( i )));
-            $('input_' + i).setValue( taskObject['taskValue']);
-            $('div_' + i).update( taskObject['taskValue'].strike());
-
-            // checks to see if task is complete and how to display it
-            if(taskObject['isCompleted'] === "true"){
-                $('input_' + i).hide()
-                $('div_' + i).show();
-            } else {
-                $('input_' + i).show()
-                $('div_' + i).hide();
-            }
-        }
-    }
-
-    // Observer for changing title
-    $('todoTitle').observe('click',function(e){
-        $('inputTitle').show();
-        $('saveTitleBtn').show();
-        $('todoTitle').hide();
-    });
-    // Observer to save title
-    $('saveTitleBtn').observe('click', function(e){
-        var titleValue = $('inputTitle').value;
-
-        $('inputTitle').hide();
-        $('saveTitleBtn').hide();
-        $('todoTitle').show();
-        $('todoTitle').update(titleValue);
-
-        localStorage.setItem('title', titleValue);
-    });
-    // obsever to add new task
-    $('addTask').observe('click', function(e) {
-       toDo.insertLi(numTask+=1);
-    });
-    // checks to see if enter key is pressed then saves the tasks
-    jQuery('#sortable').on('keyup','input', function(e) {
-        if (event.keyCode == 13) {
-            toDo.saveTasks();
-        }
-    });
-    // checks to see if finishTaskBtn is pressed then passes that element
-    jQuery('#sortable').on('click','.finishTaskBtn',function(e) {
-        toDo.finishTask(this);
-    });
-    // checks to see if deleteTaskBtn is pressed then passes that element
-    jQuery('#sortable').on('click','.deleteTaskBtn',function(e) {
-        toDo.deleteTask(this);
-    });
+    var toDo = new ToDoList(JSON.parse(localStorage.getItem('data')));
 });
+var toDoListManager = Class.create({
+    initialize: function(toDoObject) {
+        var self = this;
+        self.toDoObject = toDoObject;
+        self.titleElementId = "toDoTitle"
+        self.inputId = "input_"
+        self.taskDivId = "div_"
+        self.titleInput = "inputTitle";
+        self.liTemplate = new Template(
+            '<li id="task_#{taskId}" class="ui-state-default tasks"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>' +
+            '<input id="input_#{taskId}" size="45" type="text"><div class="taskComplete" id="div_#{taskId}"></div>' +
+            '<button class="finishTaskBtn" type="button">&#10003</button>' +
+            '<button class="deleteTaskBtn" type="button">&#10005</button></li>'
+        );
+        if(this.toDoObject === null){
+            this.toDoObject = {"title": "Click to Add Title", toDoList: [], sortOrder: ""}
+        }
 
-var ToDoList = Class.create();
-ToDoList.prototype = {
-    initialize: function() {
-    },
-    insertLi: function(numTask) {
-        // inserts list item
-        $('sortable').insert('<li  id="task_'+
-        numTask + '" class="ui-state-default tasks"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><input id="input_' +
-        numTask + '" size="45" type="text"><div class="taskComplete" id="div_' + numTask +'" ></div><button class="finishTaskBtn" type="button">&#10003</button>' +
-        '<button class="deleteTaskBtn" type="button">&#10005</button></li>')
+        self.setupHtml();
+
+        $('addTask').observe('click', function() {
+            self.addTask()
+            self.insertLi(self.toDoObject.toDoList.length - 1);
+        });
+
+        $('toDoTitle').observe('dblclick', function(){
+            $(self.titleInput).show();
+            $(self.titleElementId).hide();
+        });
+
+        $('toDoList').observe('keypress', function(event){
+            if ( event.keyCode == Event.KEY_RETURN  || event.which == Event.KEY_RETURN ) {
+                self.saveTasks();
+                Event.stop(event);
+            }
+        });
+
+        $$('.finishTaskBtn').each(function(element) {
+            element.observe('click', self.taskComplete.bindAsEventListener(self));
+        });
+
+        $$('.deleteTaskBtn').each(function(element) {
+            element.observe('click', self.deleteTask.bindAsEventListener(self));
+        });
+
+        jQuery(function() {
+            // save order of task after each reorder
+            jQuery( "#sortable" ).sortable({
+                update: function(e, ui) {
+                    //var sorted = jQuery("#sortable").sortable("serialize");
+                    //self.toDoObject.sortOrder = sorted;
+                    self.saveOrder();
+                    self.saveTasks();
+                }
+            });
+            // sets the order of the task
+            if(self.toDoObject.sortOrder != "") {
+                var sortedValues = self.toDoObject.sortOrder.substring(7).split("&task[]=");
+                var ul = jQuery("#sortable");
+                var items = jQuery("#sortable").children();
+                for (var i = sortedValues.length - 1; i >= 0; i--) {
+                    ul.prepend( items.get((sortedValues[i])));
+                }
+            }
+            jQuery( "#sortable" ).disableSelection();
+            $('inputTitle').hide();
+            $('saveTitleBtn').hide();
+        });
     },
     saveTasks: function(){
-        var taskObject;
-        localStorage.setItem('numTask', jQuery('li').length);
-        var numTask = parseInt(localStorage.getItem('numTask'));
+        var self = this;
+        var title = $(this.titleElementId).innerHTML;
 
-        // iterates through all list items and saves the data to the correct task
-        for (var i = 1; i <= numTask; i++ ){
-            taskObject =  JSON.parse(localStorage.getItem('task_' + i));
-            if( taskObject === null || taskObject['isCompleted'] === "false"){
-                taskObject = { 'taskValue' :  $('input_' + i).value, 'isCompleted' : 'false' };
-            } else {
-                taskObject = { 'taskValue' :  $('input_' + i).value, 'isCompleted' : 'true' };
-            }
-            localStorage.setItem('task_' + i, JSON.stringify(taskObject) );
+        if (!$(self.titleInput).value.empty()){
+            title = $(self.titleInput).value;
         }
-        this.saveOrder();
-        console.log('saved');
+        var toDoListJSON = {"title": title, toDoList: [], "sortOrder": ""}
 
-        jQuery('body').prepend('<div id="flash" style="display:none"></div>');
-        jQuery('#flash').html("Saved");
-        //jQuery('#flash').toggleClass('cssClassHere');
-        jQuery('#flash').slideDown('slow').fadeOut(1000);
-        //jQuery('#flash').click(function () { $('#flash').toggle('highlight') });
+        self.toDoObject.toDoList.each(function(task, index){
+            toDoListJSON.toDoList.push({
+                'task': $(self.inputId + index).getValue(),
+                'isComplete': task['isComplete']
+            });
+        })
+        toDoListJSON.sortOrder = self.toDoObject.sortOrder;
+        localStorage.setItem('data', JSON.stringify(toDoListJSON));
+        location.reload();
     },
-    deleteTask: function(child){
-        var taskLi = child.up(0);
-
-        localStorage.removeItem(taskLi.identify());
-        taskLi.remove();
-        localStorage.setItem('numTask', jQuery('li').length);
+    deleteTask: function(event){
+        var taskId = event.element().up(0).identify().replace('task_', '');
+        var taskObject = this.toDoObject.toDoList;
+        delete taskObject[taskId];
 
         this.saveOrder();
+        this.saveTasks();
     },
-    finishTask: function(child){
-        var taskID = child.up(0).identify().replace('task_','');
-        var taskObject = JSON.parse(localStorage.getItem('task_' + taskID));
+    taskComplete: function(event) {
+        var taskId = event.element().up(0).identify().replace('task_', '');
+        var taskObject = this.toDoObject.toDoList;
+        var isComplete = taskObject[taskId]['isComplete'];
 
-        if(taskObject['isCompleted'] === "true"){
-            $('input_' + taskID).show()
-            $('div_' + taskID).hide();
-            taskObject = { 'taskValue' :  $('input_' + taskID).value, 'isCompleted' : 'false' };
-            localStorage.setItem('task_' + taskID, JSON.stringify(taskObject) );
-            $('div_' + taskID).update(taskObject['taskValue'].strike());
-
-        } else {
-            $('input_' + taskID).hide()
-            $('div_' + taskID).show();
-            taskObject = { 'taskValue' :  $('input_' + taskID).value, 'isCompleted' : 'true' };
-            localStorage.setItem('task_' + taskID, JSON.stringify(taskObject) );
-            $('div_' + taskID).update(taskObject['taskValue'].strike());
+        if(isComplete){
+            taskObject[taskId]['isComplete'] = false;
+        } else{
+            taskObject[taskId]['isComplete'] = true;
         }
+        this.saveTasks();
     },
-    saveOrder: function() {
+    addTask : function() {
+        this.toDoObject.toDoList.push({
+            'task': '',
+            'isComplete': false
+        })
+        this.saveOrder();
+    },
+    saveOrder: function(){
         var sorted = jQuery("#sortable").sortable("serialize");
-        localStorage.setItem('z_sorted', sorted);
+        this.toDoObject.sortOrder = sorted;
     }
-};
+});
+
+var ToDoList = Class.create( toDoListManager,{
+    setupHtml: function() {
+        var self = this;
+        
+        $(self.titleElementId).update(self.toDoObject.title);
+
+        self.toDoObject.toDoList.each(function (task, index) {
+            self.insertLi(index);
+            if (task['isComplete']) {
+                $(self.taskDivId + index).update(task['task'].strike());
+                $(self.inputId + index).setValue(task['task']);
+                $(self.inputId + index).hide()
+                $(self.taskDivId + index).show();
+            } else {
+                $(self.inputId + index).setValue(task['task']);
+                $(self.inputId + index).show()
+                $(self.taskDivId + index).hide();
+            }
+        });
+    },
+    insertLi: function(index) {
+        var taskId = { taskId : index};
+        $('sortable').insert(this.liTemplate.evaluate(taskId));
+    }
+});
